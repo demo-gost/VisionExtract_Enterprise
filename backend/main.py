b74914d42b46ec68c131c5be54ec32862ec2f781
Sai Jannawar <saijannawar8767@gmail.com>
2026-03-27 09:17:58 +0530

    update with main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="VisionExtract Pro API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # This allows EVERY origin to connect
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
